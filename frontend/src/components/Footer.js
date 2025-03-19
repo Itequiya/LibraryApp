696f053a44e1e5413b36bdd1c47eae9034446d05
Itequiya <sadai.bp98@gmail.com>
2025-03-18 19:26:27 -0600

    feat: image resources

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,37 @@
+import "./Footer.css";
+import facebook from '../Images/facebook.png'; 
+import twitter from '../Images/twitter.png';
+import instagram from '../Images/instagram.png';
+
+
 const Footer = () => {
     return (
-      <footer>
-        <div>
-          <p>&copy; 2025 Librería. Todos los derechos reservados.</p>
+
+      <footer className="footer">
+      <div className="footer-content">
+        <p>© 2025 Itequiya. Todos los derechos reservados.</p>
+        <div className="footer-links">
+          <a href="#facebook">Facebook</a>
+          <a href="#contact">Contacto</a>
+          <a href="#privacy">Política de privacidad</a>
         </div>
-        <div>
-          <a href="">Facebook</a>
-          <a href="">Twitter</a>
-          <a href="">Instagram</a>
+
+         {/* Redes sociales con imágenes */}
+         <div className="social-links">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src={facebook} alt="Facebook" className="social-icon" />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src={twitter} alt="Twitter" className="social-icon" />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src={instagram} alt="Instagram" className="social-icon" />
+          </a>
+        
         </div>
-      </footer>
+
+      </div>
+    </footer>
     );
   };
   
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,36 @@
+
+import React, { useState } from 'react';
+import './Home.css';
+import jovenenbiblioteca from '../Images/jovenenbiblioteca.jpg';
+import jovenbiblioteca2 from '../Images/jovenbiblioteca2.jpg';
+import libreriajoven from '../Images/libreriajoven.jpg';
+
 const Home = () => {
+
+  const images = [
+    jovenbiblioteca2,
+    jovenenbiblioteca,
+    libreriajoven
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
     return (
-      <div>
-        <h2>Bienvenido a la Librería</h2>
-        <p>Aquí puedes gestionar tus préstamos de libros, ver la lista de libros disponibles y gestionar los usuarios.</p>
+      <div className="slider-container">
+      <div className="slider">
+        <img src={images[currentIndex]} alt="slider" className="slider-image" />
       </div>
+      <button className="prev-button" onClick={prevSlide}>❮</button>
+      <button className="next-button" onClick={nextSlide}>❯</button>
+    </div>
     );
   };
   
